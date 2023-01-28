f2f2b20bc3ddf8bf3620800c8fedbc6aba190bc8
JoKeR-VIKING <prathamvasani1@gmail.com>
2023-01-28 22:19:53 +0530

    resume updated

--- src/cta.js
+++ src/cta.js
@@ -4,7 +4,7 @@ class CTA extends React.Component {
     render() {
         return(
             <div className="cta">
-                <a className="btn" href="https://drive.google.com/u/0/uc?id=1frxFVrmVybyxZQimInIgZCkqxthLjJ24&export=download">Download Resume</a>
+                <a className="btn" href="https://drive.google.com/file/d/1frxFVrmVybyxZQimInIgZCkqxthLjJ24/view">Download Resume</a>
                 <a className="btn btn-primary" href="#contact">Let's Talk</a>
             </div>
         );
