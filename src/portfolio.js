566c09396218d9ed3c2f9c0aeff570363e4d1efe
JoKeR-VIKING <prathamvasani1@gmail.com>
2023-08-12 11:48:45 +0530

    new project

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,6 +9,7 @@ import minesweeper from './images/minesweeper.jfif';
 import natours from './images/natours.png';
 import nexter from './images/nexter.png';
 import trillo from './images/trillo.png';
+import sociaza from './images/sociaza.png'
 
 const projects = [
     {
@@ -16,52 +17,59 @@ const projects = [
         image: product_destination,
         title: 'Product Destination',
         github: 'https://github.com/JoKeR-VIKING/e_commerce_django',
-        live: 'https://prathamvasani1.pythonanywhere.com',
+        live: 'https://product-destination.onrender.com',
     },
     {
         id: 2,
+        image: sociaza,
+        title: 'Sociaza',
+        github: 'https://github.com/JoKeR-VIKING/Sociaza-Backend',
+        live: 'https://sociaza-app.onrender.com'
+    },
+    {
+        id: 3,
         image: natours,
         title: 'Natours',
         github: 'https://github.com/JoKeR-VIKING/Natours',
         live: 'https://joker-viking.github.io/Natours/',
     },
     {
-        id: 3,
+        id: 4,
         image: nexter,
         title: 'Nexter',
         github: 'https://github.com/JoKeR-VIKING/Nexter',
         live: 'https://joker-viking.github.io/Nexter/',
     },
     {
-        id: 4,
+        id: 5,
         image: trillo,
         title: 'Trillo',
         github: 'https://github.com/JoKeR-VIKING/Trillo',
         live: 'https://joker-viking.github.io/Trillo/',
     },
     {
-        id: 5,
+        id: 6,
         image: level_up,
         title: 'Level Up',
         github: 'https://github.com/JoKeR-VIKING/LevelUp',
-        live: 'https://levelup-r5gg.onrender.com/',
+        live: null,
     },
     {
-        id: 6,
+        id: 7,
         image: space_shooter,
         title: 'Space Shooter',
         github: 'https://github.com/JoKeR-VIKING/Space-Shooter',
         live: null,
     },
     {
-        id: 7,
+        id: 8,
         image: minesweeper,
         title: 'Minesweeper',
         github: 'https://github.com/JoKeR-VIKING/Minesweeper',
         live: null,
     },
     {
-        id: 8,
+        id: 9,
         image: fake_product_identifier,
         title: 'Fake Product Identifier',
         github: 'https://github.com/JoKeR-VIKING/FakeProductIdentifier',
@@ -86,8 +94,8 @@ class Portfolio extends React.Component {
                                     </div>
                                     <h2>{title}</h2>
                                     <div className="portfolio__item-cta">
-                                        <a className="btn" href={github} target="_blank">Github</a>
-                                        {live && <a className="btn btn-primary" href={live} target="_blank">Live Demo</a>}
+                                        <a className="btn" href={github} target="_blank" rel="noreferrer noopener">Github</a>
+                                        {live && <a className="btn btn-primary" href={live} target="_blank" rel="noreferrer noopener">Live Demo</a>}
                                     </div>
                                 </article>
                             );
